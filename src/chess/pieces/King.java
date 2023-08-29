b2bdf5a1a7bf0238b2295767eb1d77c067bc8f87
Antonio Sales <antoniogsnogueira@outlook.com>
2023-08-29 13:17:23 -0300

    ChessException and ChessPosition

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -6,10 +6,11 @@ import chess.Color;
 
 public class King extends ChessPiece {
 
+	
 	public King(Board board, Color color) {
 		super(board, color);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "k";
